b6ef45bc57b6041c73cb2a10ab93beea2dc09e3f
dapperstickman <70286076+dapperstickman@users.noreply.github.com>
2021-02-08 15:55:00 -0800

    lab4

---
+++ lab4/js/lab.js
@@ -0,0 +1,20 @@
+//Rock Paper Scissors
+//Play RPS vs AI
+//
+//Author: Phillip Guaracha and Jeremy Jacob Francisco
+//Date: 2/8/2021
+//Liscence: Whatever
+//
+//
+//
+//Prep choices for presentaion
+    // Buttons of Rock Paper and Scissors
+    //each choice has a corresponding digit
+//wait for User to input selection
+    //User selects one of three choices
+// Evaluate Data
+//Generate numbers
+    //Create numbers between 1-3
+//Select 1-3 at random
+//Select corresponding response
+//Check for Victory Condition
