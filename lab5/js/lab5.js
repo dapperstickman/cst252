ec21758075bdd74764789524cfaa1d89acbbaa80
dapperstickman <70286076+dapperstickman@users.noreply.github.com>
2021-02-10 13:12:09 -0800

    lab5

---
+++ lab5/js/lab5.js
@@ -0,0 +1,29 @@
+//Put a comment block at the top of your program
+//Rock Paper Scissors
+//Play RPS vs AI
+//
+//Author: Phillip Guaracha and Jeremy Jacob Francisco
+//Date: 2/8/2021
+//Liscence: Whatever
+//
+//
+console.log("Goteem")
+//
+//Declare all the variables you created in Task 1 in this JavaScript file
+var make = "Chervrolet";
+var model = "Van";
+var carcolor = "red";
+var carYear = "2001";
+var extras = "bad ceiling, it's ok";
+//Make it neat using lots of whitespace.
+
+//Style in programming matters, so use indentations and spaces where necessary.
+var ownIt = false;
+//Remember to put a semicolon at the end of every line.
+var age = new Date().getFullYear();
+//Use document.writeln() to neatly output each of your variables, example, document.writeln("Make: " + make + "<br>");
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Extras: " + extras + "<br>");
+document.writeln("Year: " + carYear + "<br>");
+document.writeln("Age: " + age + "<br>");
