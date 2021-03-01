4e345e74965cc337b713bdb1ec978326568fddbb
dapperstickman <70286076+dapperstickman@users.noreply.github.com>
2021-03-01 12:54:21 -0800

    coments of lab 9

--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -8,12 +8,15 @@
 //
 //
 //JavaScript for Web
+//Make new Elements
 var outputEl = document.getElementById("output");
 var new1El = document.createElement("p");
 new1El.innerHTML = ("Check This Out. Or don't.");
 var new2El = document.createElement("p");
 new2El.innerHTML = ("Would you Kindly?");
+//Linking Elements
 outputEl.appendChild(new1El);
 outputEl.appendChild(new2El);
+//Modifying Elements
 new1El.style.color = "red"
 new2El.style.fontSize = "80px"
