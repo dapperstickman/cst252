57939ce5f67edea2421751e9bb48a70d5427062a
dapperstickman <70286076+dapperstickman@users.noreply.github.com>
2021-04-07 12:55:10 -0700

    lab17

---
+++ lab17/js/lab17.js
@@ -0,0 +1,23 @@
+//Put a comment block at the top of your program
+//Lab17
+//
+//Author: Phillip Guaracha
+//Date: 4/7/2021
+//Liscence: Whatever
+//
+//
+//JavaScript for Web
+//Using JQuery
+
+//change text color of subject
+$("#subject-button").click(function(){
+	$("#sub").toggleClass("fred");
+})
+//change text color of challenge
+$("#challenge-button").click(function(){
+	$("#chal").toggleClass("george");
+})
+//change text color of results
+$("#result-button").click(function(){
+	$("#res").toggleClass("jame");
+})
